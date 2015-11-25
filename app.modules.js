b77d5b8f5d93ce754f12c6bd344e42d84c43cc4a
Sally Kingston <sally.a.kingston@gmail.com>
2015-11-24 23:56:18 -0500

    more link issues

--- app.modules.js
+++ app.modules.js
@@ -9,39 +9,39 @@
     .config(function($routeProvider){
       $routeProvider
       .when('/',{
-        templateUrl: 'views/shopper/list.html',
+        templateUrl: '#/views/shopper/list.html',
         controller: 'DreamsController'
       })
       .when('/dreams/:dreamId', {
-        templateUrl: 'views/shopper/detail.html',
+        templateUrl: '#/views/shopper/detail.html',
         controller: 'DreamsController'
       })
       .when('/dreams/:dreamId/review',{
-        templateUrl: 'views/shopper/review.html',
+        templateUrl: '#/views/shopper/review.html',
         controller: 'DreamsController'
       })
       .when('/cart',{
-        templateUrl: 'views/shopper/cart.html',
+        templateUrl: '#/views/shopper/cart.html',
         controller: 'CartController'
       })
       .when('/checkout',{
-        templateUrl: 'views/shopper/receipt.html',
+        templateUrl: '#/views/shopper/receipt.html',
         controller: 'CartController'
       })
       .when('/admin/add',{
-        templateUrl: 'views/admin/add.html',
+        templateUrl: '#/views/admin/add.html',
         controller: 'AdminController'
       })
       .when('/admin',{
-        templateUrl: 'views/admin/list.html',
+        templateUrl: '#/views/admin/list.html',
         controller: 'AdminController',
       })
       .when('/admin/:dreamId',{
-        templateUrl: 'views/admin/detail.html',
+        templateUrl: '#/views/admin/detail.html',
         controller: 'AdminController'
       })
       .when('/admin/:dreamId/edit',{
-        templateUrl: 'views/admin/edit.html',
+        templateUrl: '#/views/admin/edit.html',
         controller: 'AdminController'
       });
     });
